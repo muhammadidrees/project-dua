3e662846d1cb7eaf79cdd57c8ad12265c9ba77bb
Muhammad Idrees <midrees097@gmail.com>
2020-07-27 00:18:53 +0500

    fixed firebase issue :)

--- web/fire_app.js
+++ web/fire_app.js
@@ -1,21 +1,9 @@
-// Your web app's Firebase configuration
-var firebaseConfig = {
-    apiKey: "AIzaSyDDkzqv71V8xnzCSrAg7zhIDMbW0GoU6Ek",
-    authDomain: "duas-pwa.firebaseapp.com",
-    databaseURL: "https://duas-pwa.firebaseio.com",
-    projectId: "duas-pwa",
-    storageBucket: "duas-pwa.appspot.com",
-    messagingSenderId: "335624904391",
-    appId: "1:335624904391:web:c4f02df03dd23b0feae8e2",
-    measurementId: "G-00N5TQCWN9"
-};
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-firebase.analytics();
+importScripts("/__/firebase/7.17.1/firebase-app.js")
+importScripts("/__/firebase/7.17.1/firebase-analytics.js")
+importScripts('/__/firebase/7.17.1/firebase-messaging.js')
+importScripts("/__/firebase/init.js")
 
 const messaging = firebase.messaging()
-
-messaging.usePublicVapidKey('BIu0QBsiFbD8NdpdsydrsecvxGq8LJlDkvMgPT-Ihn3c2QF3HToVzppW5z8VAddnA6FFx19gRtNKMXuyPexzK-Q');
 
 messaging.getToken().then((currentToken) => {
     console.log(currentToken)
--- web/firebase-messaging-sw.js
+++ web/firebase-messaging-sw.js
@@ -1,18 +1,6 @@
-importScripts("https://www.gstatic.com/firebasejs/7.17.1/firebase-app.js");
-importScripts("https://www.gstatic.com/firebasejs/7.17.1/firebase-messaging.js");
-
-var firebaseConfig = {
-    apiKey: "AIzaSyDDkzqv71V8xnzCSrAg7zhIDMbW0GoU6Ek",
-    authDomain: "duas-pwa.firebaseapp.com",
-    databaseURL: "https://duas-pwa.firebaseio.com",
-    projectId: "duas-pwa",
-    storageBucket: "duas-pwa.appspot.com",
-    messagingSenderId: "335624904391",
-    appId: "1:335624904391:web:c4f02df03dd23b0feae8e2",
-    measurementId: "G-00N5TQCWN9"
-};
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
+importScripts("/__/firebase/7.17.1/firebase-app.js")
+importScripts('/__/firebase/7.17.1/firebase-messaging.js')
+importScripts("/__/firebase/init.js")
 
 const messaging = firebase.messaging()
 
@@ -23,22 +11,5 @@ messaging.setBackgroundMessageHandler(function (payload) {
         icon: '/images/icons/icon-512x512.png',
         badge: '/images/icons/icon-128x128.png'
     };
-    const promiseChain = clients
-        .matchAll({
-            type: "window",
-            includeUncontrolled: true
-        })
-        .then(windowClients => {
-            for (let i = 0; i < windowClients.length; i++) {
-                const windowClient = windowClients[i];
-                windowClient.postMessage(payload);
-            }
-        })
-        .then(() => {
-            return registration.showNotification(title, options);
-        });
-    return promiseChain;
-});
-self.addEventListener('notificationclick', function (event) {
-    console.log('notification received: ', event)
+    return self.registration.showNotification(title, options);
 });
